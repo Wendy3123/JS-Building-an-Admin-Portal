dde0cc921795ac2e28678e079fed45c2b8c9d733
Wendy Wang <wendywangg928@gmail.com>
2023-06-05 19:41:23 -0400

    updated

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
-
 // Your Code Here
+//let response = await fetch('http://localhost:3001/updateBook',{
+//    method:'PATCH',
+//    headers:{'Content-Type':'application/json'},
+//    body: JSON.stringify({
+//        'id':3,
+//        'title':'Legend of Arathrae',
+//    })
+//})
+//let updatedBook= await response.json()
+//console.log(updatedBook)
+async function loadBooks(){
+    let result = await fetch('http://localhost:3001/listBooks')        
+    
+    let books= await result.json()
+
+    books.forEach(book => {
+        let booksList = document.getElementById('books')
+
+        let listItem = document.createElement('li')
+        listItem.textContent = book.title
+
+        let listQuantity = document.createElement('input')
+        listQuantity.value = book.quantity
+
+        let saveButton = document.createElement('button')
+        saveButton.textContent = "Save"
+        saveButton.addEventListener('click',() =>{
+            fetch('http://localhost:3001/updateBook',{
+                method:'PATCH',
+                headers:{'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: listQuantity.value
+                })
+            })
+        })
+        
+        listItem.append(listQuantity,saveButton)
+
+        booksList.append(listItem)
+    })
+
+}
+loadBooks()
+
+
+
